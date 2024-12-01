dd686489b2593a207a00594771309edf2af39962
Hayden Housen <hayden@haydenhousen.com>
2024-11-30 23:33:23 -0500

    Preparing

---
+++ README.md
@@ -0,0 +1,3 @@
+# HHousen Advent Of Code 2024
+
+> HHousen's solutions to the 2024 Advent Of Code puzzles at <https://adventofcode.com/2024>.
---
+++ day_1/day1.py
@@ -0,0 +1,10 @@
+with open("puzzle_input.txt", "r") as puzzle_input:
+    puzzle_input = puzzle_input.read().splitlines()
+
+
+
+# Part 1 Solution: 
+print(f"Part 1 Solution: {part1_solution}")
+
+# Part 2 Solution: 
+print(f"Part 2 Solution: {part2_solution}")
