a105ac1c6af9434f49a5fccd0aa9deee503bb450
Hayden Housen <hayden@haydenhousen.com>
2024-12-01 00:58:08 -0500

    Complete Day 1

--- day_1/day1.py
+++ day_1/day1.py
@@ -1,10 +1,19 @@
+from collections import Counter
+
+
 with open("puzzle_input.txt", "r") as puzzle_input:
-    puzzle_input = puzzle_input.read().splitlines()
+    puzzle_input = list(map(int, puzzle_input.read().split()))
 
+nums1, nums2 = puzzle_input[0::2], puzzle_input[1::2]
+part1_solution = sum(
+    abs(num1 - num2) for num1, num2 in zip(sorted(nums1), sorted(nums2))
+)
 
-
-# Part 1 Solution: 
+# Part 1 Solution: 1660292
 print(f"Part 1 Solution: {part1_solution}")
 
-# Part 2 Solution: 
+c = Counter(nums2)
+part2_solution = sum(c[num1] * num1 for num1 in nums1)
+
+# Part 2 Solution: 22776016
 print(f"Part 2 Solution: {part2_solution}")
